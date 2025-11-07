bc14f25be74141235cd0b569027a3b461e69cf58
BARBOBZ <jordanbarbosa63112@gmail.com>
2025-11-07 11:55:57 +0100

    JeuDe.java ok, mise en place JeuPFC.java

--- src/Jeu/JeuPFC/JeuPFC.java
+++ src/Jeu/JeuPFC/JeuPFC.java
@@ -3,4 +3,26 @@ package Jeu.JeuPFC;
 import Jeu.JeuSimple;
 
 public class JeuPFC extends JeuSimple {
+
+    public JeuPFC(String nom){
+        this.nom = nom;
+    }
+
+
+    @Override
+    public int arbitrer(String c1, String c2) {
+
+        int res = 0;
+        if (Integer.parseInt(c1) > Integer.parseInt(c2)) {
+            res = -1;   //C1 Gagne
+        }
+        if (Integer.parseInt(c1) < Integer.parseInt(c2)) {
+            res = 1;    //C2 Gagne
+        }
+        return res;
+    }
+    @Override
+    public String jouerUnTour() {
+        return "";
+    }
 }
--- src/Jeu/JeuPFC/PFC.java
+++ src/Jeu/JeuPFC/PFC.java
@@ -1,4 +1,14 @@
 package Jeu.JeuPFC;
 
 public enum PFC {
+    Pierre("Pierre"),
+    Feuille("Feuille"),
+    Ciseau("Ciseau");
+
+    private String choix;
+
+    private PFC(String choix) {
+        this.choix = choix;
+    }
+
 }
